7d0cc6e7f69cb68888eac802d271eb12ead70574
eagrcode <elliot.robinson92@hotmail.com>
2024-03-17 17:31:01 +0000

    add image and Why component to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import Hero from "@/components/hero";
 import About from "@/components/about";
+import Image from "next/image";
+import Jump from "../../public/assets/images/Jump cropped.jpg";
+import Why from "@/components/why";
 
 export default function Home() {
   return (
     <main className="relative flex w-full flex-col items-center overflow-hidden">
       <Hero />
       <About />
+      <Image src={Jump} alt={"Groom & Best Men jumping"} />
+      <Why />
     </main>
   );
 }
