48479c2c43d0e4f9a3f6fa537cc778b218838678
eagrcode <elliot.robinson92@hotmail.com>
2024-03-19 14:42:49 +0000

    add responsive styling

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -3,19 +3,21 @@
 import Image from "next/image";
 import Dee from "../../public/assets/images/dee-robinson-celebrant.jpg";
 import { motion } from "framer-motion";
+import Moon from "../../public/assets/images/Hare_Navy.png";
 
 export default function about() {
   return (
-    <section className="flex w-full px-4 py-16 bg-brand-light">
-      <div className="flex flex-col w-full max-w-lg">
-        <div className="flex flex-col items-center gap-16">
+    <section className="flex w-full justify-center px-4 py-16 bg-primary md:px-12">
+      <div className="flex flex-col items-center w-full max-w-screen-lg">
+        <div className="flex flex-col items-center gap-8 md:flex-row">
           <motion.div
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
             transition={{ duration: 2 }}
-            className="w-full max-w-64"
+            className="w-full max-w-80 md:min-w-72 self-start"
           >
             <Image
+              className="shadow-md"
               src={Dee}
               alt={"Dee Robinson - Celebrant"}
               style={{ height: "auto", width: "100%" }}
@@ -25,9 +27,14 @@ export default function about() {
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
             transition={{ duration: 2 }}
-            className="flex flex-col gap-2 text-secondary"
+            className="flex flex-col gap-8 text-secondary p-4 rounded-sm"
           >
-            <h2 className="font-dancing text-4xl font-semibold">About Me</h2>
+            <div className="flex justify-between">
+              <h2 className="font-dancing text-4xl font-bold">About Me</h2>
+              <div className="flex w-8">
+                <Image src={Moon} alt={""} style={{ height: "auto", width: "100%" }} />
+              </div>
+            </div>
             <p>
               Although there is no formal requirement for a celebrant to undertake accredited
               training, it was really important to me, as a lifelong learner, to hone my craft and
