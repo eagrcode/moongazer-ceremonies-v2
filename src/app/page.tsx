69b71aba44c264ebc2feac7c111a6f1f39b15327
eagrcode <elliot.robinson92@hotmail.com>
2024-03-19 16:07:27 +0000

    add Ceremonies component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 import Hero from "@/components/hero";
 import About from "@/components/about";
-import Image from "next/image";
-import Jump from "../../public/assets/images/Jump cropped.jpg";
 import Why from "@/components/why";
+import Ceremonies from "@/components/ceremonies";
 
 export default function Home() {
   return (
     <main className="relative flex w-full flex-col items-center overflow-hidden">
       <Hero />
       <About />
-      <Image src={Jump} alt={"Groom & Best Men jumping"} />
       <Why />
+      <Ceremonies />
     </main>
   );
 }
