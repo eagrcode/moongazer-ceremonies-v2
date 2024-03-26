a88d399323b6901ffae330292ed9f0c8eabf664a
eagrcode <elliot.robinson92@hotmail.com>
2024-03-26 15:24:40 +0000

    update button styles

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,7 +19,7 @@ const buttonVariants = cva(
       size: {
         default: "h-9 px-4 py-2",
         sm: "h-8 px-3 text-xs",
-        lg: "h-12 px-10",
+        lg: "h-12 px-10 font-semibold text-base",
         icon: "h-9 w-9",
       },
     },
