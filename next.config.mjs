8db4c17c630d457542081f1b18e2efce5ea9c91f
eagrcode <elliot.robinson92@hotmail.com>
2024-04-04 16:44:55 +0100

    implement aws s3 image storage

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "moongazer-ceremonies-v2-storageda729-staging.s3.eu-west-2.amazonaws.com",
+        pathname: "/static-images/*",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
---
+++ src/lib/s3StaticImages.ts
@@ -0,0 +1,2 @@
+export const Dee: string =
+  "https://moongazer-ceremonies-v2-storageda729-staging.s3.eu-west-2.amazonaws.com/static-images/dee-robinson-celebrant.png";
