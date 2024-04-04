3959e305f88593d8bb136a2431c0e0d03b301e30
eagrcode <elliot.robinson92@hotmail.com>
2024-04-04 19:59:11 +0100

    refactor cards design

--- src/components/ceremonies.tsx
+++ src/components/ceremonies.tsx
@@ -1,14 +1,14 @@
-import CeremonySwiper from "./ceremony-swiper";
+import CeremonyCards from "./ceremony-cards";
 
 export default function ceremonies() {
   return (
-    <section className="flex w-full justify-center pr-0 bg-primary text-secondary">
-      <div className="flex flex-col gap-8 items-center w-full max-w-screen-xl">
+    <section className="flex w-full justify-center bg-primary text-secondary">
+      <div className="flex flex-col gap-8 items-center w-full max-w-screen-lg">
         <div className="flex flex-col gap-2 text-center">
           <h2>Ceremonies</h2>
           <em>view the services I offer</em>
         </div>
-        <CeremonySwiper />
+        <CeremonyCards />
       </div>
     </section>
   );
--- src/components/ceremony-cards.tsx
+++ src/components/ceremony-cards.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Wedding from "../../public/assets/images/wedding.jpg";
 import { Button } from "./ui/button";
 
-export default function CeremonySwiper() {
+export default function CeremonyCards() {
   const ceremonies = [
     {
       id: 1,
@@ -43,17 +43,25 @@ export default function CeremonySwiper() {
   ];
 
   return (
-    <div className="flex w-full gap-4 overflow-scroll scrollbar-hide snap-mandatory snap-x text-secondary">
+    <div className="flex flex-col items-center w-full gap-4 text-secondary md:grid grid-cols-6 md:justify-center">
       {ceremonies.map((item, index) => (
         <div
           key={index}
-          className="flex flex-col min-w-[80%] justify-center items-center snap-center rounded-sm sm:min-w-[70%] md:min-w-[60%] lg:min-w-[40%] xl:min-w-0 xl:[&:nth-child(2)]:mt-28 xl:[&:nth-child(4)]:mt-28"
+          className={`flex flex-col w-full max-w-[400px] items-center rounded-sm col-span-2 ${
+            ceremonies.length === 5 && index === 3 && "md:col-start-2 md:col-span-2"
+          } ${ceremonies.length === 5 && index === 4 && "md:col-start-4 md:col-span-2"}`}
         >
-          <div className="flex w-full">
-            <Image src={item.img} alt={item.alt} style={{ height: "auto", width: "100%" }} />
+          <div className="flex w-full flex-1">
+            <Image
+              src={item.img}
+              alt={item.alt}
+              height={200}
+              width={350}
+              style={{ objectFit: "contain" }}
+            />
           </div>
 
-          <div className="flex flex-col w-full gap-8 text-center items-center justify-center py-8  bg-secondary/10 border-b-2 border-l-2 border-r-2 border-secondary/10">
+          <div className="flex-1 flex flex-col w-full h-full gap-8 text-center items-center justify-center py-8  bg-secondary/10 border-b-2 border-l-2 border-r-2 border-secondary/10">
             <h2 className="text-secondary">{item.title}</h2>
             <Button asChild size="lg">
               <Link href={item.path}>Learn More</Link>
