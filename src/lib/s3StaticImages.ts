716a258ac75daedc10f63a86ab05b4ce48b73b06
eagrcode <elliot.robinson92@hotmail.com>
2024-04-04 20:21:20 +0100

    add remaining s3 urls

--- src/lib/s3StaticImages.ts
+++ src/lib/s3StaticImages.ts
@@ -1,2 +1,40 @@
-export const Dee: string =
-  "https://moongazer-ceremonies-v2-storageda729-staging.s3.eu-west-2.amazonaws.com/static-images/dee-robinson-celebrant.png";
+type urlType = string;
+
+const baseURL =
+  "https://moongazer-ceremonies-v2-storageda729-staging.s3.eu-west-2.amazonaws.com/static-images";
+
+export const Dee: urlType = `${baseURL}/dee-robinson-celebrant.png`;
+
+export const HareHero: urlType = `${baseURL}/Logo_Navy_500.png`;
+
+export const Jump: urlType = `${baseURL}/Jump+cropped.jpg`;
+
+export const Planning: urlType = `${baseURL}/wedding-list.jpg`;
+
+export const Wedding: urlType = `${baseURL}/wedding.jpg`;
+
+export const Funeral: urlType = `${baseURL}/funeral.jpg`;
+
+export const Naming: urlType = `${baseURL}/naming-ceremony.jpg`;
+
+export const Commitment: urlType = `${baseURL}/commitment-ceremony.jpg`;
+
+export const Renewal: urlType = `${baseURL}/renewal.jpg`;
+
+export const Grid1: urlType = `${baseURL}/Grid-1.jpg`;
+
+export const Grid2: urlType = `${baseURL}/Grid-2.jpg`;
+
+export const Grid3: urlType = `${baseURL}/Grid-3.jpg`;
+
+export const Grid4: urlType = `${baseURL}/Grid-4.jpg`;
+
+export const AOIC: urlType = `${baseURL}/AOIC-logo.jpg`;
+
+export const CCL: urlType = `${baseURL}/CCL-logo.jpg`;
+
+export const FCC: urlType = `${baseURL}/fcc-badge.png`;
+
+export const MoonBlack: urlType = `${baseURL}/Moon_Black.png`;
+
+export const MoonWhite: urlType = `${baseURL}/Moon_White.png`;
