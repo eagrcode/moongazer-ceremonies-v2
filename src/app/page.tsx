178351d233269f3dfefa0d112ef8a8c014195f37
eagrcode <elliot.robinson92@hotmail.com>
2024-04-05 14:23:52 +0100

    change component name due to not recognising former change or capital letter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import About from "@/components/about";
 import Why from "@/components/why";
 import Ceremonies from "@/components/ceremonies";
 import Planning from "@/components/planning";
-import Testimonials from "@/components/testimonials";
+import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
   return (
