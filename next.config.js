185ac1545d26e741102e118ad6ddd49a9d3b1105
eagrcode <elliot.robinson92@hotmail.com>
2024-04-08 20:31:46 +0100

    add rule to remove logs during production

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
   images: {
     remotePatterns: [
       {
