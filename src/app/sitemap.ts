406b43a507bea30cdba3ab62f2b955720cfe6551
eagrcode <elliot.robinson92@hotmail.com>
2024-04-14 15:56:11 +0100

    fix incorrect url

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,49 +1,49 @@
 import { MetadataRoute } from "next";
 
-const baseURL = "https://moongazerceremonies.com";
+const baseURL = "https://moongazerceremonies.co.uk";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: baseURL,
       lastModified: new Date(),
-      changeFrequency: "weekly",
+      changeFrequency: "hourly",
       priority: 1,
     },
     {
       url: `${baseURL}/wedding`,
       lastModified: new Date(),
-      changeFrequency: "weekly",
+      changeFrequency: "hourly",
       priority: 1,
     },
     {
       url: `${baseURL}/renewal`,
       lastModified: new Date(),
-      changeFrequency: "weekly",
+      changeFrequency: "hourly",
       priority: 1,
     },
     {
       url: `${baseURL}/naming`,
       lastModified: new Date(),
-      changeFrequency: "weekly",
+      changeFrequency: "hourly",
       priority: 1,
     },
     {
       url: `${baseURL}/funeral`,
       lastModified: new Date(),
-      changeFrequency: "weekly",
+      changeFrequency: "hourly",
       priority: 1,
     },
     {
       url: `${baseURL}/commitment`,
       lastModified: new Date(),
-      changeFrequency: "weekly",
+      changeFrequency: "hourly",
       priority: 1,
     },
     {
       url: `${baseURL}/contact`,
       lastModified: new Date(),
-      changeFrequency: "weekly",
+      changeFrequency: "hourly",
       priority: 1,
     },
   ];
