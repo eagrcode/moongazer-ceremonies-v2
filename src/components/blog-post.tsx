5d7a56c25e0a9af099e5a60c48225bfb7dc28018
Elliot Robinson <elliot.robinson92@hotmail.com>
2024-12-01 14:43:30 +0000

    restructure for responsive styling

--- src/components/blog-post.tsx
+++ src/components/blog-post.tsx
@@ -6,30 +6,36 @@ import Link from "next/link";
 export default function BlogPost({ blogPost }: { blogPost: SingleBlogPost }) {
   return (
     <div className="flex flex-col gap-4 max-w-4xl text-base">
-      <div className="flex justify-between pb-4 gap-4 border-b-2 border-b-secondary/30">
-        <div className="flex flex-col w-full justify-between gap-4">
-          <Link className="text-sm hover:text-gray-950" href={"/blog"}>
-            {"< Back to posts"}
-          </Link>
-          <div className="flex flex-col gap-4">
-            <p className="text-sm">
-              {formatDate(new Date(blogPost?.created_at))}
-              {" | "}
-              {blogPost.categories.category}
-            </p>
-            <h1 className="flex text-4xl font-serif font-normal">
-              {blogPost?.title}
-            </h1>
-            <p className="text-sm">Posted by Dee</p>
+      <div className="flex flex-col justify-between pb-4 gap-4 border-b-2 border-b-secondary/30">
+        <Link className="text-sm hover:text-gray-950" href={"/blog"}>
+          {"< Back to posts"}
+        </Link>
+        <div className="flex flex-col gap-4 lg:flex-row-reverse">
+          <div className="flex w-full items-end max-w-[450px]">
+            <Image
+              className="rounded-md object-cover"
+              src={blogPost.image_url || ""}
+              alt={""}
+              height={176}
+              width={250}
+              placeholder="blur"
+              blurDataURL="blur"
+            />
           </div>
-        </div>
-        <div className="relative flex w-full items-end max-w-[350px]">
-          <Image
-            className="rounded-md object-contain"
-            src={blogPost.image_url || ""}
-            alt={""}
-            fill
-          />
+
+          <div className="flex flex-col w-full justify-between gap-4 lg:justify-end">
+            <div className="flex flex-col gap-4">
+              <p className="text-sm">
+                {formatDate(new Date(blogPost?.created_at))}
+                {" | "}
+                {blogPost.categories.category}
+              </p>
+              <h1 className="flex text-4xl font-serif font-normal">
+                {blogPost?.title}
+              </h1>
+              <p className="text-sm">Posted by Dee</p>
+            </div>
+          </div>
         </div>
       </div>
 
